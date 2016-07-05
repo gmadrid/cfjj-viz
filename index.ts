e1a1e29255f06ac6d1425f2d2cfed1ff6029d854
George Madrid <gmadrid@gmail.com>
2016-07-05 10:30:26 -0400

    Importing d3. Works.

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
+import * as d3 from 'd3';
 import greeter from './greeter';
 import $ = require('jquery');
 
 $(() => {
   $(document.body).html(greeter('World'));
+
+  var body = d3.select('body');
+  var div = body.append('div');
+  div.html('Hello, d3js');
 });
