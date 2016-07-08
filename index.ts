940e502cacfe1f370b1aca0e8ae3fb9be4a5e34e
George Madrid <gmadrid@gmail.com>
2016-07-08 14:04:25 -0400

    Re-factor code to be MVI style.

--- index.ts
+++ index.ts
@@ -8,18 +8,41 @@ import {GenerateD3Chart} from './d3chart';
 
 const buttonName = '#randomButton'
 
-function main(sources) {
-  let data$ = sources.DOM.select(buttonName).events('click')
-    .map(_ => { return generateRandomData(); })
-    .startWith(generateRandomData());
+function intent(sources) {
+  let generateData$ = sources.DOM.select(buttonName).events('click').startWith(1);
+  return {
+    generateData$: generateData$
+  }
+}
 
-  const sinks = {
-    DOM: xs.of(div([
+function model(intent) {
+  let data$ = intent.generateData$
+    .map(_ => { return generateRandomData(); });
+    
+  return {
+    data$: data$
+  }
+}
+
+function htmlView(_) {
+  return xs.of(div([
       div([input(buttonName, {attrs: {type:'button', value: 'Generate random data'}})]),
       div([
         h('svg#d3svg')
-      ])])),
-    D3: data$
+      ])]));
+}
+
+function d3View(m) {
+  return m.data$;
+}
+
+function main(sources) {
+  let i = intent(sources);
+  let m = model(i);
+
+  const sinks = {
+    DOM: htmlView(m),
+    D3: d3View(m)
   };
   return sinks;
 }
