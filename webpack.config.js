0359af7ca778bacdbfcb6dca3a9861f451f28f00
George Madrid <gmadrid@gmail.com>
2016-07-13 09:31:53 -0400

    Putting dist files in dist/ directory.

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+
 module.exports = {
   entry: './src/index.ts',
   output: {
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    path: 'dist'
   },
   devtool: 'source-map',
   resolve: {
     extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      title: 'CfJJ Visualization'
+    })
 //    new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
     loaders: [
-      { test: /\.ts$/, loader: 'ts-loader'}
+      { test: /\.ts$/, loader: 'ts'}
     ]
   }
 }
